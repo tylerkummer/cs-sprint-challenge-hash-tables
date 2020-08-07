c7323bf26b87df41d4e7140b123fb8e1a7713271
Tyler Kummer <tylerkummer@protonmail.com>
2020-08-07 12:33:58 -0500

    ex4 works with hash table

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -4,36 +4,50 @@ def has_negatives(a):
     """
     # Your code here
 
+    # Didn't use hash table
+
+    # # Create empty array for our results, positive and negative numbers
+    # result = []
+    # positive = []
+    # negative = []
+
+    # # Cycle through our array
+    # for i in a:
+    #     # Check if it is a positive number
+    #     if i > 0:
+    #         # Append it to our positive array
+    #         positive.append(i)
+    #     else:
+    #         # Else append the absolute value to our negative array
+    #         negative.append(abs(i))
+
+    # # Loop through our positive array
+    # for i in positive:
+    #     # Check if i exists in negative
+    #     if i in negative:
+    #         # Check if i does not exist in the result
+    #         if i not in result:
+    #             # Append i to the result
+    #             result.append(i)
+
+    # Uses hash table
+
+    # Create a hash table
     cache = {}
-
-    # Create empty array for our results, positive and negative numbers
+    # Create a result array
     result = []
-    positive = []
-    negative = []
 
-    # Cycle through our array
+    # Cycle through a
     for i in a:
-        # Check if it exists in the cache
-        if i not in cache:
-            # Check if it is a positive number
-            if i > 0:
-                # Append it to our positive array
-                positive.append(i)
-            else:
-                # Else append the absolute value to our negative array
-                negative.append(abs(i))
+        # Check if the absolute value is in the cache
+        if abs(i) in cache:
+            # If it is then we have a match for both positive and negative numbers, so append it to the result array
+            result.append(abs(i))
+            # Set the cache of the abs of i to increment by 1
+            cache[abs(i)] += 1
+        # If the absolute value is not in cache then just asign it to 1
         else:
-            # If it doesn't exist in cache asign the cache of i to i
-            cache[i] = i
-
-    # Loop through our positive array
-    for i in positive:
-        # Check if i exists in negative
-        if i in negative:
-            # Check if i does not exist in the result
-            if i not in result:
-                # Append i to the result
-                result.append(i)
+            cache[abs(i)] = 1
 
     return result
 
