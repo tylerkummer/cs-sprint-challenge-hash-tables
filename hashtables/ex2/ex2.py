091a62344605289829a6f413174d9dd37d4b80d9
Tyler Kummer <tylerkummer@protonmail.com>
2020-08-07 11:13:29 -0500

    ex2 works

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -11,4 +11,22 @@ def reconstruct_trip(tickets, length):
     """
     # Your code here
 
-    # return route
+    # Create a cache for our hash table
+    cache = {}
+    # Create our list with our length of tickets
+    route = [None] * length
+
+    # Cycle through all of our tickets
+    for i in tickets:
+        # Assign source as key and destination as value
+        cache[i.source] = i.destination
+
+    # Make our first index of route the one that has a source of "NONE"
+    route[0] = cache["NONE"]
+
+    # Cycle through all the of our length to give us an index value, skipping 0 from earlier
+    for i in range(1, length):
+        # Find the next ticket based on the previous location and add it to our route
+        route[i] = cache[route[i - 1]]
+
+    return route
