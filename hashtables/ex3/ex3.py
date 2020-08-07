a228d54deffeda1d975c5a6ffae42fc0fb140f7d
Tyler Kummer <tylerkummer@protonmail.com>
2020-08-07 12:50:28 -0500

    ex3 works

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -4,6 +4,28 @@ def intersection(arrays):
     """
     # Your code here
 
+    # Create hash table
+    cache = {}
+
+    # Create an empty array for our intersected list
+    result = []
+
+    # Cycle through our array with arrays
+    for i in arrays:
+        # Cycle through each individual array within our array of arrays
+        for j in i:
+            # Check if the individual number j is located in our hash table cache then
+            if j in cache:
+                # Increment the counter of cache by 1
+                cache[j] += 1
+                # Check if the the number in j is also in the rest of the arrays
+                if cache[j] == len(arrays):
+                    # If so then append our result array by j
+                    result.append(j)
+            else:
+                # If not then just assign the cache of j to 1
+                cache[j] = 1
+
     return result
 
 
